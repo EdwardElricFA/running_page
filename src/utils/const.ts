5d56aa01d0cbd731befe64d26006839271047252
EdwardElricFA <lostinaddiction@gmail.com>
2024-04-27 21:07:58 +0800

    Update const.ts

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -99,10 +99,12 @@ export {
 
 const nike = 'rgb(224,237,94)'; // if you want change the main color change here src/styles/variables.scss
 const dark_vanilla = 'rgb(228,212,220)';
+const HANAASAGI = 'rgb(30,136,168)';
+
 
 // If your map has an offset please change this line
 // issues #92 and #198
 export const NEED_FIX_MAP = false;
-export const MAIN_COLOR = nike;
+export const MAIN_COLOR = HANAASAGI;
 export const PROVINCE_FILL_COLOR = '#47b8e0';
 export const COUNTRY_FILL_COLOR = dark_vanilla;
