469a006a6609f13ef73e01245809599cfaaa97c1
EdwardElricFA <lostinaddiction@gmail.com>
2024-05-03 22:27:50 +0800

    yellow

--- src/components/LocationStat/index.tsx
+++ src/components/LocationStat/index.tsx
@@ -31,7 +31,7 @@ const LocationStat = ({
         Yesterday you said tomorrow.
       </p>
     </section>
-    <hr color="red" />
+    <hr color="yellow" />
     <LocationSummary />
     <CitiesStat onClick={changeCity} />
     <PeriodStat onClick={changeTitle} />
