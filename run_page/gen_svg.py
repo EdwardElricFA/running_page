b4e72da63ef593c129151dcb7e9df10ea06f9a0d
EdwardElricFA <lostinaddiction@gmail.com>
2024-05-03 19:35:57 +0800

    Update gen_svg.py

--- run_page/gen_svg.py
+++ run_page/gen_svg.py
@@ -106,6 +106,7 @@ def main():
         dest="track_color2",
         metavar="COLOR",
         type=str,
+        default="#FFB11B",
         help="Secondary color of tracks (default: none).",
     )
     args_parser.add_argument(
